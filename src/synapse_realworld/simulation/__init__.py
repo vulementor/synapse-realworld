6a9f45a128afa5f8eb8376434988c5de641958a8
vulementor <vulementor@gmail.com>
2026-09-15 11:23:19 +0700

    feat: export scenario uncertainty APIs

--- src/synapse_realworld/simulation/__init__.py
+++ src/synapse_realworld/simulation/__init__.py
@@ -1,4 +1,20 @@
 from synapse_realworld.simulation.engine import RealWorldSimulator
 from synapse_realworld.simulation.models import Scenario, SimulationResult
+from synapse_realworld.simulation.uncertainty import (
+    QuantileSummary,
+    ScenarioUncertaintyResult,
+    simulate_registered_model_uncertainty,
+    simulate_scenario_uncertainty,
+    summarize,
+)
 
-__all__ = ["RealWorldSimulator", "Scenario", "SimulationResult"]
+__all__ = [
+    "QuantileSummary",
+    "RealWorldSimulator",
+    "Scenario",
+    "ScenarioUncertaintyResult",
+    "SimulationResult",
+    "simulate_registered_model_uncertainty",
+    "simulate_scenario_uncertainty",
+    "summarize",
+]
