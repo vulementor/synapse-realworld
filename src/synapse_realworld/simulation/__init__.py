7ecb22e27ca020108681a5708ff2f691ef0a2eb2
vulementor <vulementor@gmail.com>
2026-09-15 11:24:58 +0700

    feat: export real-data simulator factories

--- src/synapse_realworld/simulation/__init__.py
+++ src/synapse_realworld/simulation/__init__.py
@@ -1,4 +1,8 @@
 from synapse_realworld.simulation.engine import RealWorldSimulator
+from synapse_realworld.simulation.factory import (
+    build_real_data_simulator,
+    build_temporal_choice_set_factory,
+)
 from synapse_realworld.simulation.models import Scenario, SimulationResult
 from synapse_realworld.simulation.uncertainty import (
     QuantileSummary,
@@ -14,6 +18,8 @@ __all__ = [
     "Scenario",
     "ScenarioUncertaintyResult",
     "SimulationResult",
+    "build_real_data_simulator",
+    "build_temporal_choice_set_factory",
     "simulate_registered_model_uncertainty",
     "simulate_scenario_uncertainty",
     "summarize",
