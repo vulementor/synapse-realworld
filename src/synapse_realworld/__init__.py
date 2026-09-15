ba34b68ec3560522f8786ea3b999475bac2707ae
vulementor <vulementor@gmail.com>
2026-09-15 10:20:58 +0700

    feat: add package public API

---
+++ src/synapse_realworld/__init__.py
@@ -0,0 +1,7 @@
+"""Synapse Real-World Platform public package."""
+
+from synapse_realworld.simulation.engine import RealWorldSimulator
+from synapse_realworld.simulation.models import Scenario, SimulationResult
+
+__all__ = ["RealWorldSimulator", "Scenario", "SimulationResult"]
+__version__ = "0.1.0"
