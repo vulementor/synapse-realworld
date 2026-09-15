79158cfc2ae698bb9e0bd2e10456fd8f415a6880
vulementor <vulementor@gmail.com>
2026-09-15 19:31:57 +0700

    feat: export experiment quality API

--- src/synapse_realworld/experiments/__init__.py
+++ src/synapse_realworld/experiments/__init__.py
@@ -9,6 +9,10 @@ from synapse_realworld.experiments.models import (
     VariantAggregate,
 )
 from synapse_realworld.experiments.prediction import prediction_from_scenario_comparison
+from synapse_realworld.experiments.qa import (
+    ExperimentQualityAssessment,
+    assess_experiment_quality,
+)
 from synapse_realworld.experiments.registry import FileExperimentRegistry
 from synapse_realworld.experiments.scorecard import (
     ModelExperimentScorecard,
@@ -21,10 +25,12 @@ __all__ = [
     "ExperimentEvaluation",
     "ExperimentObservation",
     "ExperimentPrediction",
+    "ExperimentQualityAssessment",
     "ExperimentStatus",
     "FileExperimentRegistry",
     "ModelExperimentScorecard",
     "VariantAggregate",
+    "assess_experiment_quality",
     "build_model_experiment_scorecard",
     "evaluate_experiment",
     "prediction_from_scenario_comparison",
