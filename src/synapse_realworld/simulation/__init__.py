7b85a49c17f277bf03cba9ba38a3f92dfad7b746
vulementor <vulementor@gmail.com>
2026-09-15 10:22:33 +0700

    feat: expose simulation API

---
+++ src/synapse_realworld/simulation/__init__.py
@@ -0,0 +1,4 @@
+from synapse_realworld.simulation.engine import RealWorldSimulator
+from synapse_realworld.simulation.models import Scenario, SimulationResult
+
+__all__ = ["RealWorldSimulator", "Scenario", "SimulationResult"]
