504549839e66da1ff094737769fe34a51517c7d0
vulementor <vulementor@gmail.com>
2026-09-15 10:57:49 +0700

    feat: export training dataset helpers

--- src/synapse_realworld/data/__init__.py
+++ src/synapse_realworld/data/__init__.py
@@ -4,12 +4,22 @@ from synapse_realworld.data.projection import (
     project_reason_observations,
 )
 from synapse_realworld.data.quality import DataQualityError, QualityIssue, validate_choice_set
+from synapse_realworld.data.training import (
+    TimedCalibrationExample,
+    build_calibration_examples,
+    household_from_decision_row,
+    temporal_holdout,
+)
 
 __all__ = [
     "DataQualityError",
     "HistoricalDecisionAssembler",
     "QualityIssue",
+    "TimedCalibrationExample",
+    "build_calibration_examples",
+    "household_from_decision_row",
     "project_feature_observations",
     "project_reason_observations",
+    "temporal_holdout",
     "validate_choice_set",
 ]
