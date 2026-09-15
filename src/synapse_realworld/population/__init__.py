35c88f2142b1f0222d5dc1810ca1571b79d09a7f
vulementor <vulementor@gmail.com>
2026-09-15 11:20:17 +0700

    feat: export empirical population fitting APIs

--- src/synapse_realworld/population/__init__.py
+++ src/synapse_realworld/population/__init__.py
@@ -1,3 +1,22 @@
+from synapse_realworld.population.empirical import (
+    EmpiricalPopulationGenerator,
+    EmpiricalPopulationProfile,
+    MarginalComparison,
+    NumericComparison,
+    PopulationDiagnostics,
+    diagnose_population,
+    fit_empirical_population_profile,
+)
 from synapse_realworld.population.generator import PopulationProfile, SyntheticPopulationGenerator
 
-__all__ = ["PopulationProfile", "SyntheticPopulationGenerator"]
+__all__ = [
+    "EmpiricalPopulationGenerator",
+    "EmpiricalPopulationProfile",
+    "MarginalComparison",
+    "NumericComparison",
+    "PopulationDiagnostics",
+    "PopulationProfile",
+    "SyntheticPopulationGenerator",
+    "diagnose_population",
+    "fit_empirical_population_profile",
+]
