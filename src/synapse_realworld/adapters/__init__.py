9cc8a322b99994330cd3339d7eaca590c55c35d4
vulementor <vulementor@gmail.com>
2026-09-15 10:52:39 +0700

    fix: export sales capture row iterator

--- src/synapse_realworld/adapters/__init__.py
+++ src/synapse_realworld/adapters/__init__.py
@@ -1,11 +1,13 @@
 from synapse_realworld.adapters.csv_canonical import load_offers_csv, load_unit_versions_csv
 from synapse_realworld.adapters.jsonl_events import load_events_jsonl, write_events_jsonl
 from synapse_realworld.adapters.sales_capture import (
+    iter_sales_capture_rows,
     load_sales_capture_csv,
     sales_capture_row_to_events,
 )
 
 __all__ = [
+    "iter_sales_capture_rows",
     "load_events_jsonl",
     "load_offers_csv",
     "load_sales_capture_csv",
