009de8285f5f4c7088256ab8ea9fc377834c6567
vulementor <vulementor@gmail.com>
2026-09-15 10:50:28 +0700

    feat: export ingestion pipeline

---
+++ src/synapse_realworld/ingestion/__init__.py
@@ -0,0 +1,4 @@
+from synapse_realworld.ingestion.models import IngestionResult
+from synapse_realworld.ingestion.pipeline import EventIngestor, build_snapshot
+
+__all__ = ["EventIngestor", "IngestionResult", "build_snapshot"]
