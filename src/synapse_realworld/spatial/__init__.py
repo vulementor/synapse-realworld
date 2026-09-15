17b091362669e7f1e45f58a8da8d02f7ef29f240
vulementor <vulementor@gmail.com>
2026-09-15 11:13:54 +0700

    feat: export leakage-safe spatial projection APIs

--- src/synapse_realworld/spatial/__init__.py
+++ src/synapse_realworld/spatial/__init__.py
@@ -1,7 +1,13 @@
+from synapse_realworld.spatial.projection import (
+    build_project_commute_provider,
+    project_household_workplace_anchors,
+)
 from synapse_realworld.spatial.travel_time import (
     DepartureBucket,
+    HouseholdAnchorObservation,
     HouseholdCommuteProvider,
     LocationAnchor,
+    TemporalHouseholdAnchorIndex,
     TemporalTravelTimeIndex,
     TravelMode,
     TravelTimeObservation,
@@ -9,9 +15,13 @@ from synapse_realworld.spatial.travel_time import (
 
 __all__ = [
     "DepartureBucket",
+    "HouseholdAnchorObservation",
     "HouseholdCommuteProvider",
     "LocationAnchor",
+    "TemporalHouseholdAnchorIndex",
     "TemporalTravelTimeIndex",
     "TravelMode",
     "TravelTimeObservation",
+    "build_project_commute_provider",
+    "project_household_workplace_anchors",
 ]
