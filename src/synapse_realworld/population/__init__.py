4bf570527273433651dbe3a14f057541654539e2
vulementor <vulementor@gmail.com>
2026-09-15 11:22:20 +0700

    feat: export population protocol and ID-free prototypes

--- src/synapse_realworld/population/__init__.py
+++ src/synapse_realworld/population/__init__.py
@@ -1,6 +1,7 @@
 from synapse_realworld.population.empirical import (
     EmpiricalPopulationGenerator,
     EmpiricalPopulationProfile,
+    HouseholdPrototype,
     MarginalComparison,
     NumericComparison,
     PopulationDiagnostics,
@@ -8,13 +9,16 @@ from synapse_realworld.population.empirical import (
     fit_empirical_population_profile,
 )
 from synapse_realworld.population.generator import PopulationProfile, SyntheticPopulationGenerator
+from synapse_realworld.population.ports import PopulationGenerator
 
 __all__ = [
     "EmpiricalPopulationGenerator",
     "EmpiricalPopulationProfile",
+    "HouseholdPrototype",
     "MarginalComparison",
     "NumericComparison",
     "PopulationDiagnostics",
+    "PopulationGenerator",
     "PopulationProfile",
     "SyntheticPopulationGenerator",
     "diagnose_population",
