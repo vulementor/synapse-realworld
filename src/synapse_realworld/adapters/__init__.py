2405e70636fdb930531c17b8835708dd6a52e9e1
vulementor <vulementor@gmail.com>
2026-09-15 10:52:11 +0700

    feat: export canonical JSONL adapters

--- src/synapse_realworld/adapters/__init__.py
+++ src/synapse_realworld/adapters/__init__.py
@@ -1,12 +1,15 @@
 from synapse_realworld.adapters.csv_canonical import load_offers_csv, load_unit_versions_csv
+from synapse_realworld.adapters.jsonl_events import load_events_jsonl, write_events_jsonl
 from synapse_realworld.adapters.sales_capture import (
     load_sales_capture_csv,
     sales_capture_row_to_events,
 )
 
 __all__ = [
+    "load_events_jsonl",
     "load_offers_csv",
     "load_sales_capture_csv",
     "load_unit_versions_csv",
     "sales_capture_row_to_events",
+    "write_events_jsonl",
 ]
