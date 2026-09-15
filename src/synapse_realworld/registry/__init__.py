8970725533a4f490c6d02c08f9517f3992ccd6db
vulementor <vulementor@gmail.com>
2026-09-15 11:09:00 +0700

    feat: export model registry APIs

---
+++ src/synapse_realworld/registry/__init__.py
@@ -0,0 +1,17 @@
+from synapse_realworld.registry.file_registry import FileModelRegistry
+from synapse_realworld.registry.models import (
+    ModelArtifact,
+    ModelDecision,
+    ModelStatus,
+    RegisteredModel,
+)
+from synapse_realworld.registry.ports import ModelRegistry
+
+__all__ = [
+    "FileModelRegistry",
+    "ModelArtifact",
+    "ModelDecision",
+    "ModelRegistry",
+    "ModelStatus",
+    "RegisteredModel",
+]
