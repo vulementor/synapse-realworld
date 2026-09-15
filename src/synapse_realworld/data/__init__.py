166064bc36de585164d9e2518db415b2b57e8d41
vulementor <vulementor@gmail.com>
2026-09-15 10:57:09 +0700

    feat: export projection and decision assembly APIs

--- src/synapse_realworld/data/__init__.py
+++ src/synapse_realworld/data/__init__.py
@@ -1,3 +1,15 @@
+from synapse_realworld.data.decision_assembler import HistoricalDecisionAssembler
+from synapse_realworld.data.projection import (
+    project_feature_observations,
+    project_reason_observations,
+)
 from synapse_realworld.data.quality import DataQualityError, QualityIssue, validate_choice_set
 
-__all__ = ["DataQualityError", "QualityIssue", "validate_choice_set"]
+__all__ = [
+    "DataQualityError",
+    "HistoricalDecisionAssembler",
+    "QualityIssue",
+    "project_feature_observations",
+    "project_reason_observations",
+    "validate_choice_set",
+]
