7f546c3990a90ffcf43d9dd69dfdfe94503c9e84
vulementor <vulementor@gmail.com>
2026-09-15 10:51:32 +0700

    feat: export calibration API

--- src/synapse_realworld/behaviour/__init__.py
+++ src/synapse_realworld/behaviour/__init__.py
@@ -1,3 +1,16 @@
+from synapse_realworld.behaviour.calibration import (
+    CalibrationExample,
+    CalibrationMetrics,
+    CalibrationResult,
+    MultinomialLogitCalibrator,
+)
 from synapse_realworld.behaviour.utility import BaselineUtilityModel, UtilityWeights
 
-__all__ = ["BaselineUtilityModel", "UtilityWeights"]
+__all__ = [
+    "BaselineUtilityModel",
+    "CalibrationExample",
+    "CalibrationMetrics",
+    "CalibrationResult",
+    "MultinomialLogitCalibrator",
+    "UtilityWeights",
+]
