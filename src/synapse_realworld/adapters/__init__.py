c97f6158f11c760f1262942afc9ef588e2610e29
vulementor <vulementor@gmail.com>
2026-09-15 10:58:02 +0700

    feat: export outcome and declarative mapping adapters

--- src/synapse_realworld/adapters/__init__.py
+++ src/synapse_realworld/adapters/__init__.py
@@ -1,6 +1,8 @@
 from synapse_realworld.adapters.csv_canonical import load_offers_csv, load_unit_versions_csv
 from synapse_realworld.adapters.inventory_events import offer_to_event, unit_version_to_event
 from synapse_realworld.adapters.jsonl_events import load_events_jsonl, write_events_jsonl
+from synapse_realworld.adapters.mapping import EventMappingSpec, FieldRule, map_record_to_event
+from synapse_realworld.adapters.outcomes import load_outcomes_csv, outcome_row_to_event
 from synapse_realworld.adapters.sales_capture import (
     iter_sales_capture_rows,
     load_sales_capture_csv,
@@ -8,12 +10,17 @@ from synapse_realworld.adapters.sales_capture import (
 )
 
 __all__ = [
+    "EventMappingSpec",
+    "FieldRule",
     "iter_sales_capture_rows",
     "load_events_jsonl",
     "load_offers_csv",
+    "load_outcomes_csv",
     "load_sales_capture_csv",
     "load_unit_versions_csv",
+    "map_record_to_event",
     "offer_to_event",
+    "outcome_row_to_event",
     "sales_capture_row_to_events",
     "unit_version_to_event",
     "write_events_jsonl",
