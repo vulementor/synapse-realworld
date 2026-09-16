16af08d51668a9013999fd152865c148541c92f8
vulementor <vulementor@gmail.com>
2026-09-16 11:45:39 +0700

    feat: export LAA dataset snapshot APIs

---
+++ src/synapse_realworld/datasets/__init__.py
@@ -0,0 +1,15 @@
+from synapse_realworld.datasets.snapshot import (
+    FileFingerprint,
+    LAARealDatasetSnapshotManifest,
+    create_laa_real_dataset_snapshot,
+    load_laa_snapshot_manifest,
+    verify_laa_snapshot_directory,
+)
+
+__all__ = [
+    "FileFingerprint",
+    "LAARealDatasetSnapshotManifest",
+    "create_laa_real_dataset_snapshot",
+    "load_laa_snapshot_manifest",
+    "verify_laa_snapshot_directory",
+]
